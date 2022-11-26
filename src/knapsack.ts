ada3b54e5c35c90b6bd7006629a915272011df97
Alon Faingertz <afaingertz@atlassian.com>
2022-11-26 11:38:29 +1100

    refactor code naming

--- src/knapsack.ts
+++ src/knapsack.ts
@@ -1,22 +1,28 @@
 import { Item, Pack, Result } from './types';
 
 export function knapsack({ maximumWeight, items }: Pack): Result {
-  const maxItemIndex = items.length - 1;
-  const totalCapacity = maximumWeight;
-  const memo: number[] = [];
+  const itemsLength = items.length - 1;
+  const arrMap = new Map<number, number>();
 
   function value(itemIndex: number, capacity: number) {
     if (itemIndex < 0 || capacity <= 0) {
       return 0;
     }
-
-    const key = totalCapacity * itemIndex + (capacity - 1);
-    if (!!memo[key]) {
-      return memo[key];
+    const key = maximumWeight * itemIndex + (capacity - 1);
+    if (arrMap.has(key)) {
+      return arrMap.get(key)!;
     }
-    return (memo[key] = calculateValue(itemIndex, capacity));
+    const val = calculateValue(itemIndex, capacity);
+    arrMap.set(key, val);
+    return val;
   }
 
+  /**
+   * Get the
+   * @param itemIndex
+   * @param capacity
+   * @returns
+   */
   function calculateValue(itemIndex: number, capacity: number): number {
     const { weight: itemCost, cost: itemValue } = items[itemIndex];
     const vPrevious = value(itemIndex - 1, capacity);
@@ -27,25 +33,21 @@ export function knapsack({ maximumWeight, items }: Pack): Result {
     return vCombined > vPrevious ? vCombined : vPrevious;
   }
 
-  function select() {
-    const bag: Item[] = [];
-    for (let i = maxItemIndex, capacity = totalCapacity; i >= 0; i -= 1) {
+  function getItems() {
+    const pack: Item[] = [];
+    for (let i = itemsLength, capacity = maximumWeight; i >= 0; i -= 1) {
       if (value(i, capacity) <= value(i - 1, capacity)) {
         continue;
       }
       const item = items[i];
       capacity -= item.weight;
-      bag.push(item);
+      pack.push(item);
     }
-    return bag;
+    return pack;
   }
 
   return {
-    getMaxCost: () => {
-      return value(maxItemIndex, totalCapacity);
-    },
-    getItems: select,
+    maxCost: value(itemsLength, maximumWeight),
+    items: getItems(),
   };
 }
-
-export const maximumValue: (pack: Pack) => number = (pack) => knapsack(pack).getMaxCost();
